2cd578cbb06d5e33203371f4aaf1f7c06b8e61ff
chadirahme <eng.chadi@gmail.com>
2019-12-25 17:29:00 -0500

    change home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
                 <h4 className="App-title">{this.state.message}</h4>
               </header>
               <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.123
+                To get started, edit <code>src/App.js</code> and save to reload.AWS
               </p>
             </div>
         );
